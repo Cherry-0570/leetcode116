ad5fc1a012fa635026ec13be603ce02bc6105f77
25A31A0570 <25a31a0570@pragati.ac.in>
2025-12-24 20:16:34 +0530

    Time: 0 ms (100%), Space: 12.3 MB (47.33%) - LeetHub

---
+++ 2942-find-words-containing-character/2942-find-words-containing-character.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int* findWordsContaining(char** words, int wordsSize, char x, int* returnSize) {
+    int* result = (int*)malloc(wordsSize * sizeof(int));
+    int count = 0;
+
+    for (int i = 0; i < wordsSize; i++) {
+        for (int j = 0; words[i][j] != '\0'; j++) {
+            if (words[i][j] == x) {
+                result[count++] = i;
+                break;  
+            }
+        }
+    }
+
+    *returnSize = count;
+    return result;
+}
