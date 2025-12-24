fe82a52794b079cb4a459e850eaff9c2c59d4172
25A31A0570 <25a31a0570@pragati.ac.in>
2025-12-24 20:24:41 +0530

    Time: 9 ms (95.89%), Space: 10.7 MB (83.56%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+int compare(const void* a, const void* b) {
+    int x = *(const int*)a;
+    int y = *(const int*)b;
+    return x - y;
+}
+
+int arrayPairSum(int* nums, int numsSize) {
+    qsort(nums, numsSize, sizeof(int), compare);
+    int sum = 0;
+    for (int i = 0; i < numsSize; i += 2) {
+        sum += nums[i];  
+    }
+    return sum;
+}
